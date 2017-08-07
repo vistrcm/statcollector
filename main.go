d9a177ca73d2d22a2e73100f96083386abcb2d79
Stanislav Vitko <svitko@griddynamics.com>
2017-08-06 22:49:45 -0700

    play with chi

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gopkg.in/mgo.v2"
+	"html"
+	"net/http"
+	"time"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func createRecord(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Created")
+}
 
 func main() {
-	fmt.Println("hello")
+	// get Mongo Session
+	session, err := mgo.Dial("mongodb://collector:Ci1aTh1ooshiib6iepha4oongaeSho@localhost/stats")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	r := chi.NewRouter()
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	//r.Use(middleware.RedirectSlashes)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", index)
+
+	// RESTy routes for "data" resource
+	r.Route("/data", func(r chi.Router) {
+		r.Get("/", index)
+		r.Post("/", createRecord)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
